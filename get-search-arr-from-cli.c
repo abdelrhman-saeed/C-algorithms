ce3b6240c0a1d18cfc8551b74debaafcc527e9e1
abdelrhman-saeed <abdelrhmanSaed001@gmail.com>
2022-09-08 23:12:25 +0200

    modified get-search-arr-from-cli.c file for more clean code

--- get-search-arr-from-cli.c
+++ get-search-arr-from-cli.c
@@ -4,43 +4,44 @@
 #include <stdlib.h>
 #include <stdbool.h>
 
-int *get_search_arr_from_cli(int argc, char* argv[]) {
+
+void prerequestes(int argc, int search_nums_len) {
 
     if (argc < 3) {
-
         printf("you need to write a number to search for and a set of numbers you want to search in,"
                 "seperated by a ','\nexample: 2 \"3,4,6,72,3,4,62,2\"\n");
-        exit(0);
+        exit(1);
     }
 
-    // getting the count of numbers to declare an array of nums with the coutn number
+    if (search_nums_len < 2) {
+        printf("your search array is not well formed, its should be like : \nexample : \"3,4,6,72,3,4,62,2\"\n");
+        exit(1);
+    }
+}
+
+
+int get_search_arr_len(char search_arr_as_text[]) {
+
     int search_nums_len = 0;
 
-    for(int i = 0; i < strlen(argv[2]); i++) {
-        if (argv[2][i] == ',') search_nums_len++;
+    for(int i = 0; i < strlen(search_arr_as_text); i++) {
+        if (search_arr_as_text[i] == ',') search_nums_len++;
     }
 
-    if ( ! search_nums_len) {
-        printf("your search array is not well formed, its should be like : \nexample : \"3,4,6,72,3,4,62,2\"\n");
-        exit(0);
-    }
+    return ++search_nums_len;
+}
 
-    search_nums_len++; // increasing the length by one to match the array size
 
-    int *search_nums = (int*) malloc(search_nums_len  * sizeof(int));
-    /**
-     * i didn't find another way to pass the arr length actually
-     * so the length is the first index, and the search array is after that
-     */
-    search_nums[0] = search_nums_len;
+int *get_search_arr_from_cli(char search_arr_as_text[], int search_nums_len) {
 
-	char *splited_nums = strtok(argv[2], ",");
+    int *search_nums    = (int*) malloc(search_nums_len  * sizeof(int));
+	char *splited_nums  = strtok(search_arr_as_text, ",");
 
-    for(int i = 1; i < search_nums_len; i++) {
+    for(int i = 0; i < search_nums_len; i++)
+    {
         search_nums[i]  = atoi(splited_nums);
         splited_nums    = strtok(NULL, ",");
     }
 
-    // search_nums[search_nums_len] = search_nums_len;
     return search_nums;
 }
