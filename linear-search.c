badb321ba011d29dd96001f6bd69782d5fa43f9c
abdelrhman-saeed <abdelrhmanSaed001@gmail.com>
2022-09-09 05:15:53 +0200

    modified the linear-search.c file to take its inputs like "search array" and "the target to search for" from cli

--- linear-search.c
+++ linear-search.c
@@ -4,25 +4,32 @@
 #include <stdlib.h>
 
 
-// looking for the num 0 in the array [1,2,3,4,5,6,7,8,9,0]
+void prerequestes(int argc, int search_nums_len);
 
-int main(int argc, char* argv[]) {
+int get_search_arr_len(char search_arr_as_text[]);
+int *get_search_arr_from_cli(char search_arr_as_text[], int search_nums_len);
 
-    if (argc < 2) {
-        printf("you need to write a number to search for\n");
-        return false;
-    }
 
-    int search_num  = atoi(argv[1]); // converting the number from char to int
-    int nums[]      = {1,2,3,4,5,6,7,8,9,0};
-    
-    size_t nums_len = sizeof(nums) / sizeof(nums[0]);
+bool search(int nums[], int nums_len , int target) {
 
-    for(int i = 0; i < nums_len; i++) {
-        if (nums[i] == search_num) {
+    for(int i = 0; i < nums_len; i++)
+    {
+        if (nums[i] == target) {
             printf("Found, the index of your number is : %i\n", i);
-            return true;
+            return 0;
         }
     }
     printf("Not Found\n");
+    return 1;
+
+}
+
+int main(int argc, char* argv[]) {
+
+    int nums_len = get_search_arr_len(argv[2]); // passing the search array from the cli, formated as "1,2,3,4,5"
+    prerequestes(argc, nums_len); // executing the prerequests so if the input is invalid formatted the execution stops with a warning
+
+    int * nums = get_search_arr_from_cli(argv[2], nums_len); // passing the search array with its length
+
+    search(nums, nums_len, atoi(argv[1])); // executing the linear search algorithm
 }
